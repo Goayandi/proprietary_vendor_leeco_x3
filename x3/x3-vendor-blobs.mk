7db3a585af2fec35f1590618d505d487254b21fe
vishalk17 <vishalkapadi95@gmail.com>
2018-02-18 19:34:29 +0000

    reback to the original location of etc/

--- x3/x3-vendor-blobs.mk
+++ x3/x3-vendor-blobs.mk
@@ -1,4 +1,6 @@
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/vendor,system/vendor)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/etc,system/etc)
+
 #app folder files
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/leeco/x3/proprietary/app/LetvRemoteControl_preinstall/lib,system/app/LetvRemoteControl_preinstall/lib)
